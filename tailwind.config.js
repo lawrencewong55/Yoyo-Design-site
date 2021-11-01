5bb51a848ac59fd70fd1172f7f6e4a92c96b489f
lawrencewong55 <lawrencewong55@gmail.com>
2021-11-01 23:16:26 +0000

    Section-7 completed
    Completed 'Lets stay in touch' section

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,12 @@ module.exports = {
   purge: ['./dist/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+
     extend: {
       spacing: {
         '18': '4.5rem',
         '86': '22.5rem',
+        '64.5': '16.25rem',
       }
     },
   },
