f5c1517fb430ae6c12c1a556924b1e60d4942737
lawrencewong55 <lawrencewong55@gmail.com>
2021-11-02 00:20:24 +0000

    footer completed
    Added headings for easier navigation in code.
    
    Finished Footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@ module.exports = {
   purge: ['./dist/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-
+    backgroundColor: theme => ({
+      ...theme('colors'),
+      'blackGrey': '#1f1f1f',}),
+    
     extend: {
       spacing: {
         '18': '4.5rem',
