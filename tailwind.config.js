051f294eb987ff1b77604894d65e49a0bc346eed
lawrencewong55 <lawrencewong55@gmail.com>
2021-11-04 01:14:40 +0000

    responsive 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,5 +31,25 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    function ({ addComponents }) {
+      addComponents({
+        '.container': {
+          maxWidth: '100%',
+          '@screen sm': {
+            maxWidth: '600px',
+          },
+          '@screen md': {
+            maxWidth: '700px',
+          },
+          '@screen lg': {
+            maxWidth: '800px',
+          },
+          '@screen xl': {
+            maxWidth: '900px',
+          },
+        }
+      })
+    }
+  ],
 };
